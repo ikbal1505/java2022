2d6a9eb73373492add8435fb3fb73ec291c9e50c
ikbal1505 <ikbal1505@users.noreply.github.com>
2022-09-26 15:50:40 +0300

    İkinciOdev

---
+++ inheritance/src/CustomerManager.java
@@ -0,0 +1,10 @@
+public class CustomerManager extends PersonManager{
+    public void list()
+    {
+        System.out.println("Listelendi");
+    }
+    public void add()
+    {
+        System.out.println("Eklendi");
+    }
+}
